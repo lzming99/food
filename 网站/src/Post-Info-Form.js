1c40adcba671dceeb4e8a525318cb1f2a7e04038
Li Ziming <46721076+Ziming99@users.noreply.github.com>
2019-07-27 12:55:35 +0800

    =.=

--- 网站/src/Post-Info-Form.js
+++ 网站/src/Post-Info-Form.js
@@ -1,5 +1,11 @@
 // JavaScript Document
 function PostSubmit() {
 	var postUrl = url; // submit url 
-	var data 
+	var data1 = name;
+	var data2 = date;
+	
+	var ExportForm = document.createElement('form');
+	document.body.appendChild(ExportForm);
+	ExportForm.method = 'post';
+	var newElement = document
 }
